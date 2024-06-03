42622b5972af909d9df1f7f31213b85f354af031
wxxhyeong <wxxhyeong@gmail.com>
2024-06-04 02:36:19 +0900

    add : 조우형/week1/boj1541

--- src/조우형/week1/boj1758_wh.java
+++ src/조우형/week1/boj1758_wh.java
@@ -1,2 +1,33 @@
-package 조우형.week1;public class boj1758_wh {
+package 조우형.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class boj1758_wh {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long result = 0;
+        Integer[] arr = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            long tip = arr[i] - i;
+            if (tip > 0) {
+                result += tip;
+            }
+        }
+
+        System.out.println(result);
+    }
 }
