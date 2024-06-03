dd0440580d62179f85713bfdc306c28ddfed7917
장성훈 <57349859+jsh9611@users.noreply.github.com>
2024-06-04 08:32:19 +0900

    장성훈/week1/boj1758

---
+++ src/장성훈/week1/boj1758_sh.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr,(o1, o2) -> o2 - o1);
+
+        long answer = 0;
+        for (int i = 0; i < n; i++) {
+            int temp = arr[i] - i;
+            if (temp < 0) { break; }
+
+            answer += temp;
+        }
+
+        System.out.println(answer);
+    }
+}
+//Arrays.sort(int[]) 에는 Comparator 를 입력받는 오버로드가 없기 때문에 Comparator 를 사용할 수 없습니다.
+//Integer[] 를 사용할 때에는 Arrays.sort(T[], Comparator<? super T>) 를 호출할 수 있습니다.
